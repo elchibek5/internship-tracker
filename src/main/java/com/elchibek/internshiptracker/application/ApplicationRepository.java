136c0bef4644bf8410d0251d3feea7abd8c70c8b
elchibekdastanov <elchibekdastanov@gmail.com>
2025-12-27 17:20:31 -0600

    .

--- src/main/java/com/elchibek/internshiptracker/application/ApplicationRepository.java
+++ src/main/java/com/elchibek/internshiptracker/application/ApplicationRepository.java
@@ -1,14 +1,28 @@
 package com.elchibek.internshiptracker.application;
 
+import com.elchibek.internshiptracker.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
-    Page<Application> findByStatus(ApplicationStatus status, Pageable pageable);
+    Optional<Application> findByIdAndUser(Long id, User user);
 
-    Page<Application> findByCompanyContainingIgnoreCase(String company, Pageable pageable);
+    Page<Application> findByUser(User user, Pageable pageable);
 
-    Page<Application> findByStatusAndCompanyContainingIgnoreCase(ApplicationStatus status, String company, Pageable pageable);
+    Page<Application> findByUserAndStatus(User user, ApplicationStatus status, Pageable pageable);
+
+    Page<Application> findByUserAndCompanyContainingIgnoreCase(User user, String company, Pageable pageable);
+
+    Page<Application> findByUserAndStatusAndCompanyContainingIgnoreCase(
+            User user,
+            ApplicationStatus status,
+            String company,
+            Pageable pageable
+    );
+
+    void deleteByIdAndUser(Long id, User user);
 }
