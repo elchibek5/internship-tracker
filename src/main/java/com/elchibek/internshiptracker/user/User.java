c6f7c0b765593b140590f16ebec5f8a51b970e7e
elchibekdastanov <elchibekdastanov@gmail.com>
2025-12-28 23:41:35 -0600

    .

---
+++ src/main/java/com/elchibek/internshiptracker/user/User.java
@@ -0,0 +1,23 @@
+package com.elchibek.internshiptracker.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+}
